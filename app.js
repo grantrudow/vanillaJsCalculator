b3c80149b1e97505d877d7cae306522a742344b4
Grant Rudow <webdev.grant@gmail.com>
2021-10-05 13:12:34 -0400

    finished functionality

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-const clear = document.querySelector('#clear');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-const divide = document.querySelector('#divide');
-const multiply = document.querySelector('#multiply');
-const subtract = document.querySelector('#subtract');
-const add = document.querySelector('#add');
-const equals = document.querySelector('#equals');
+// Variable
+let currentInput = document.querySelector('.currentInput');
+let answerScreen = document.querySelector('.answerScreen');
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+
+// Calculate display
+let realTimeScreenValue = []
+
+// Clear display
+
+clearbtn.addEventListener('click', function() {
+    realTimeScreenValue = [''];
+    answerScreen.innerHTML = 0;
+    currentInput.className = 'currentInput';
+    answerScreen.className = 'answerScreen';
+    answerScreen.style.color = " rgba(150, 150, 150, 0.87)";
+})
+
+// Get value of any button clicked and display to the screen
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        // when clicked button is not erase button
+        if(!btn.id.match('erase')) {
+            // Display value of btn pressed
+            realTimeScreenValue.push(btn.value)
+            currentInput.innerHTML = realTimeScreenValue.join('');
+
+            // Evaluate answer in real time
+            if (btn.classList.contains('num_btn')) {
+                answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+            }
+        }
+
+        // When erase button pressed
+        if (btn.id.match('erase')) {
+            realTimeScreenValue.pop();
+            currentInput.innerHTML = realTimeScreenValue.join('');
+            answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
+        }
+
+        // When evaluate button pressed
+        if (btn.id.match('evaluate')) {
+            currentInput.className = 'answerScreen';
+            answerScreen.className = 'currentInput';
+            answerScreen.style.color = 'white';
+        }
+
+        // Prevent undefined error screen
+        if (typeof eval(realTimeScreenValue.join('')) == 'undefined') {
+            answerScreen.innerHTML = 0;
+        }
+    })
+})
